a9e469974667af09442793b83b1644fc0569f5e5
hcronan3 <hcronan@stu.jsu.edu>
2018-05-22 11:16:19 -0500

    updated gencyber index
    updated some bootstrap stuff and used cards for more responsiveness. Also added template top to use across site

--- wwwroot/sites/gencyber/index.php
+++ wwwroot/sites/gencyber/index.php
@@ -1,68 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
- <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require ('../templateTop.php');
+?>
 
-   <!-- Bootstrap 4 Dependencies -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="module-page.css">
-</head>
 
-<body>
 <div class="jumbotron text-center">
  <h1>GenCyber Modules</h1>
 </div>
-
+<div class="container">
 <div class="row">
-  <div class="col"></div>
     <?php
     module_col('Module 1', 'This is an example of a module description.');
     module_col('Module 2', 'This is an example of a module description.');
     module_col('Module 3', 'This is an example of a module description.');
     module_col('Module 4', 'This is an example of a module description.');
-    ?>
-  <div class="col"></div>
-</div>
-
-<br>
-
-<div class="row">
-  <div class="col"></div>
-    <?php
     module_col('Module 5', 'This is an example of a module description.');
     module_col('Module 6', 'This is an example of a module description.');
     module_col('Module 7', 'This is an example of a module description.');
     module_col('Module 8', 'This is an example of a module description.');
     ?>
-  <div class="col"></div>
+  
 </div>
 
+
+
+<div class="row">
+  
+    <?php
+  
+    ?>
+  
+</div>
+</div>
 </body>
 </html>
 
 <?php
   function module_col($mod_title, $mod_discription) {
-    echo '
-      <div class="col-md-2">
-        <div class="panel panel-default text-center">
-
-          <div class="panel-heading">
-            <h2>' . $mod_title . '</h2>
-          </div>
-
-          <div class="panel-body">
-            <p style="text-align:left;">
-              ' . $mod_discription . '
-            </p>
-          </div>
-
-        <div class="panel-footer">
-          <button class="btn btnlg">View Unit</button>
-          <button class="btn btnlg">Lesson Plans</button>
+    echo '<div class="col-xs-12 col-sm-6 col-md-4">
+      <div class="card">
+        <div class="card-header red">
+          <h4>'.$mod_title.'</h4>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">Lorem Ipsum</h5>
+          <p class="card-text">'.$mod_discription.'</p>
+          <a href="#" class="btn btn-dark">Learn</a>
+          <a href="#" class="btn btn-dark">Hands On</a>
         </div>
       </div>
     </div>';
