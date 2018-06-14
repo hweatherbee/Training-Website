aa5d53c08613cdc6eaf1ffd51a64f55dbbfdd18f
hcronan3 <hcronan@stu.jsu.edu>
2018-06-14 10:57:18 -0500

    gencyber mod 1 template page

--- wwwroot/sites/gencyber/index.php
+++ wwwroot/sites/gencyber/index.php
@@ -8,14 +8,14 @@ require ('templateTop.php');
 <div class="row">
   
     <?php
-    module_col('Module 1', 'This is an example of a module description.');
-    module_col('Module 2', 'This is an example of a module description.');
-    module_col('Module 3', 'This is an example of a module description.');
-    module_col('Module 4', 'This is an example of a module description.');
-    module_col('Module 5', 'This is an example of a module description.');
-    module_col('Module 6', 'This is an example of a module description.');
-    module_col('Module 7', 'This is an example of a module description.');
-    module_col('Module 8', 'This is an example of a module description.');
+    module_col('Module 1', 'This is an example of a module description.', 'gcmod1');
+    module_col('Module 2', 'This is an example of a module description.', 'gcmod2');
+    module_col('Module 3', 'This is an example of a module description.', 'gcmod3');
+    module_col('Module 4', 'This is an example of a module description.', 'gcmod4');
+    module_col('Module 5', 'This is an example of a module description.', 'gcmod5');
+    module_col('Module 6', 'This is an example of a module description.', 'gcmod6');
+    module_col('Module 7', 'This is an example of a module description.', 'gcmod7');
+    module_col('Module 8', 'This is an example of a module description.', 'gcmod8');
     ?>
 
 </div>
@@ -27,14 +27,14 @@ require ('templateTop.php');
 </html>
 
 <?php
-  function module_col($mod_title, $mod_discription) {
+  function module_col($mod_title, $mod_discription, $mod_link) {
     echo '
       <div class="col-md-3">
         <div class="card">
         <h5 class="card-header">'.$mod_title.'</h5>
         <div class="card-body"> 
           <p class="card-text">'.$mod_discription.'</p>
-          <a href="#" class="btn btn-primary">View Module</a>
+          <a href="'.$mod_link.'.php" class="btn btn-primary">View Module</a>
         </div>
       </div>
     </div>';
