9887772311cb541fa7b8e9fc45c7c68fcb419b71
hcronan3 <hcronan@stu.jsu.edu>
2018-06-14 11:16:47 -0500

    navbar changes

--- wwwroot/sites/gencyber/templateTop.php
+++ wwwroot/sites/gencyber/templateTop.php
@@ -14,15 +14,15 @@
 
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href=""><img src="../../resources/gencyber-logo.png" style="width: 45%;"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
+    <!-- <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="">GenCyber <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
@@ -41,8 +41,8 @@
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
+    </ul> -->
+    <form class="form-inline my-2 my-lg-0 ml-auto" >
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
